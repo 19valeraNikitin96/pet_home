c7ae54124f0dfa13eb15965198932ab5ffa78fa4
valerii <19valeranikitin96@gmail.com>
2022-04-02 11:26:11 +0300

    Added creation of additional mock user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,14 @@ INSERT INTO USERS (id, username, password, person_id) VALUES
   (99999, 'Bogdan1980', '12345', 99999);
 INSERT INTO ADVERTISEMENTS (id, pet_name, age, type, location_id, date_id, owner_id) VALUES
   (99999, 'Jerry', 3, 'FOUND', 99999, 99999, 99999);
+
+INSERT INTO LOCATIONS (id, city, district, street) VALUES
+  (99998, 'Kharkiv', 'Kyivskyi', 'Industrialna');
+INSERT INTO DATES (id, day, month, year) VALUES
+  (99998, 4, 5, 2021);
+INSERT INTO PERSONS (id, firstname, lastname, location_id) VALUES
+  (99998, 'Andrii', 'Bilous', 99998);
+INSERT INTO USERS (id, username, password, person_id) VALUES
+  (99998, 'user123', '54321', 99998);
+INSERT INTO ADVERTISEMENTS (id, pet_name, age, type, location_id, date_id, owner_id) VALUES
+  (99998, 'Noname', 1, 'FOUND', 99998, 99998, 99998);
