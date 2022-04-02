3802043ebce9add3daebcb6fe933ef0386d8590b
valerii <19valeranikitin96@gmail.com>
2022-04-02 12:10:36 +0300

    Changed getting of URL parameters

--- pet_home_testing/pet_home_testing/__init__.py
+++ pet_home_testing/pet_home_testing/__init__.py
@@ -1,8 +1,10 @@
+import os
+
 
 class App(object):
-    protocol = 'http'
-    addr = '127.0.0.1'  # localhost
-    port = 8080
+    protocol = os.environ.get('PROTOCOL', 'http')
+    addr = os.environ.get('ADDR', '127.0.0.1')  # localhost
+    port = os.environ.get('PORT', '8080')
 
     def get_url_prefix(self) -> str:
         return f"{self.protocol}://{self.addr}:{self.port}/"
